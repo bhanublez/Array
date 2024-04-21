114497f30bba4644055ff12c72142730e184bea6
Bhanu Pratap Singh <114097203+bhanublez@users.noreply.github.com>
2024-04-21 08:00:54 +0530

    Update lss.c
    correct one

--- lss.c
+++ lss.c
@@ -1,67 +1,82 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+//longest sub sequence using recursion
 #include <stdio.h>
+#include <string.h>
 
-int max(int i,int j){
-    return i>j?i:j;
-}
-int dp[4][4];
+#define MAX_LEN 1000
 
-int lcm(char arr1[],char arr2[],int i,int j){
-    if(i==0 || j==0){
+int dp[MAX_LEN + 1][MAX_LEN + 1];
+int n, m;
+
+int longestCommonSubsequence(char *text1, char *text2, int i, int j) {
+    if (i == 0 || j == 0) {
         return 0;
     }
     
-    if(arr1[i]==arr2[j]){
-        if(dp[i][j]==-1){
-            dp[i][j]=1+lcm(arr1,arr2,i-1,j-1);
-        }else{
-            return dp[i][j];
-        }
-        return dp[i][j];
-    }else{
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        dp[i][j]=max(lcm(arr1,arr2,i-1,j),lcm(arr1,arr2,i,j-1));
+    if (dp[i][j] != -1) {
         return dp[i][j];
     }
+    
+    if (text1[i - 1] == text2[j - 1]) {
+        dp[i][j] = 1 + longestCommonSubsequence(text1, text2, i - 1, j - 1);
+    } else {
+        int lcs1 = longestCommonSubsequence(text1, text2, i - 1, j);
+        int lcs2 = longestCommonSubsequence(text1, text2, i, j - 1);
+        dp[i][j] = (lcs1 > lcs2) ? lcs1 : lcs2;
+    }
+    
+    return dp[i][j];
 }
 
-int main()
-{
-    char arr1[]={'a','c','c'};
-    char arr2[]={'a','c','a'};
-   
-   
+int main() {
+    char text1[] = "abcde";
+    char text2[] = "ace";
     
+    n = strlen(text1);
+    m = strlen(text2);
     
-    //now by 
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            if(i==0||j==0){
-                dp[i][j]==0;
-            }else{
-                dp[i][j]=-1;
-            }
-        }    
-    }
-     int n=lcm(arr1,arr2,3,3);
-      printf("n %d",n);
-      
-      printf("\n");
+    memset(dp, -1, sizeof(dp));
     
-     for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            printf(" %d ",dp[i][j]);
-        }printf("\n");
-     }
-
+    int lcsLength = longestCommonSubsequence(text1, text2, n, m);
+    printf("%d\n", lcsLength);
+    
     return 0;
 }
+
+//using tabulization or say  iterative approach
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 1000
+
+int dp[MAX_LEN + 1][MAX_LEN + 1];
+int n, m;
+
+int longestCommonSubsequence(char *text1, char *text2) {
+    n = strlen(text1);
+    m = strlen(text2);
+    
+    memset(dp, 0, sizeof(dp));
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = (dp[i - 1][j] > dp[i][j - 1]) ? dp[i - 1][j] : dp[i][j - 1];
+            }
+        }
+    }
+    
+    return dp[n][m];
+}
+
+int main() {
+    char text1[] = "abcde";
+    char text2[] = "ace";
+    
+    int lcsLength = longestCommonSubsequence(text1, text2);
+    printf("%d\n", lcsLength);
+    
+    return 0;
+}
+
